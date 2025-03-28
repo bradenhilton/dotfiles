214bc8d89d057eb0afc8c24eddff3bc169c848ea
Braden Hilton <hiltonbraden@gmail.com>
2025-03-28 11:15:04 +0000

    powershell: Update self-elevate.ps1

--- home/.chezmoitemplates/powershell/self-elevate.ps1
+++ home/.chezmoitemplates/powershell/self-elevate.ps1
@@ -1,16 +1,25 @@
 [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Provides a boolean value for use in script templates')]
 $isElevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
+function Test-IsSudoEnabled {
+    $key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Sudo'
+    $value = 'Enabled'
+
+    if (-not (Test-Path -Path $key -PathType Container)) { return $false }
+    if (-not (Get-ItemProperty -Path $key -Name $value)) { return $false }
+
+    return [bool](Get-ItemPropertyValue -Path $key -Name $value)
+}
+
 function Invoke-Elevated {
     param (
         [System.IO.FileInfo]$Path
     )
 
-    $arguments = @('-NoLogo', '-NoProfile', '-NonInteractive', '-File', "`"$Path`"")
-    # $process = Start-Process -FilePath 'powershell' -ArgumentList $arguments -Verb 'RunAs' -WindowStyle ([System.Diagnostics.ProcessWindowStyle]::Hidden) -PassThru -Wait
-    # $global:LASTEXITCODE = $process.ExitCode
-    if (Get-Command -Name 'sudo' -CommandType Application -ErrorAction SilentlyContinue) {
-        sudo --preserve-env powershell @arguments
+    $arguments = @('-NoLogo', '-NoProfile', '-NonInteractive', '-ExecutionPolicy', 'RemoteSigned', '-File', "`"$Path`"")
+
+    if (Test-IsSudoEnabled) {
+        & sudo --preserve-env powershell @arguments
     } else {
         Start-Process -FilePath 'powershell' -ArgumentList $arguments -Verb RunAs -WindowStyle Hidden -PassThru -Wait
         exit $process.ExitCode
